47c083bd3131ef0acba8dde9d1ab5c6259ff09c8
TheoWinterhalter <isheeft@gmail.com>
2016-04-07 00:13:27 +0200

    Started Interface for html

---
+++ src/html.mli
@@ -0,0 +1,50 @@
+type media_value =
+  | Media_all
+  | Media_aural
+  | Media_braille
+  | Media_handheld
+  | Media_projection
+  | Media_print
+  | Media_screen
+  | Media_tty
+  | Media_tv
+  | Media_min_width of string (* TODO A type for int + unit *)
+  (* TODO *)
+
+type media =
+  | Media_or  of media * media
+  | Media_and of media * media
+  | Media_not of media
+  | Media_val of media_value
+
+type rel_value =
+  | Rel_icon
+  | Rel_stylesheet
+  (* Are the others necessary? *)
+
+type link
+val link : ?href:  string -> (* Some better idea? *)
+           ?media: media ->
+           ~rel:   rel_value ->
+           link
+
+type head
+val head : (* ?styles: style list -> *)
+           (* ?base: ? -> *)
+           ?links: link list ->
+           (* ?metas: meta list -> *)
+           (* ?scripts: scrupt list -> *)
+           (* ?noscripts? *)
+           ~title: string ->
+           head
+
+type body
+type 'a element
+
+val p : ('a element -> 'a) element
+
+val body : 'a element -> 'a
+val body_end : body element
+
+type html
+val html : head -> body -> html
